19a56f5b77be1187ba877c582252f120b53ebc2d
admin <losh.oleg.12@gmail.com>
2024-03-14 21:00:21 +0000

    final commit2.0

--- src/js/script.js
+++ src/js/script.js
@@ -19,6 +19,80 @@ percent.forEach( (item, i) => {
 })
 
 
+$(document).ready(function(){
+
+    
+
+
+    $('#contacts').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            checkbox: {
+                required: true
+            }
+
+        },
+        messages: {
+            name: {
+                required: "Please enter your name",
+                minlength: jQuery.validator.format("Enter {0} character!")
+            },
+            checkbox:{ 
+                required:"Click to agree"
+            },
+            email: {
+                required: "Please enter your email",
+                email: "Incorrect mail address entered"
+            }
+        }
+    });
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+
+        openWait();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input, textarea").val("");
+
+
+            $('form').trigger('reset');
+            closeWait();
+            openThanks(); 
+        });
+        return false;
+    });
+
+    function openWait() {
+        $('.overlay, #wait').fadeIn('slow');
+    }
+    function openThanks() {
+        $('.overlay, #thanks').fadeIn('slow');
+    }
+    function closeWait() {
+        $('.overlay, #wait').fadeOut('slow');
+    }
+
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #thanks').fadeOut('slow');
+    });
+
+});
 
 
 
